c0e9e38e87c8bd0a85e6f2c82c976855a26921b3
BorisiukIvan <121752518+BorisiukIvan@users.noreply.github.com>
2023-01-01 16:19:18 +0100

    Update and rename make_bot.py to upgrade.py

---
+++ upgrade.py
@@ -0,0 +1,15 @@
+# To play, you must upgrade your lichess fork account to bot first!
+import requests
+
+# Enter here Lichess clone, were your bot is registered (e.g. https://lichess.org)
+site = "https://lichess.org"
+
+# Enter here your API token (generate one on https://your-clone.org/account/oauth/token)
+# Note that if account played at least 1 game before upgrating, you will get an error from server!
+api = "Bearer GrKdhWV5rrYAu0tX"
+
+# Sending our request..
+r = requests.post("https://lidraughts.org/api/bot/account/upgrade", headers = {"Authorization" : api})
+
+# Printing result
+print(r.status_code, r.text)
